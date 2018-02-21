dd8f379c1b91ae68defe76cb8efd753020da0dd5
JackieW001 <jwoo@stuy.edu>
2018-02-20 21:22:50 -0500

    added db queries

--- catalan.py
+++ catalan.py
@@ -18,5 +18,35 @@ def parse_json (filename):
 
 pprint(parse_json(filename))
 
-#collection.insert_one()
+'''
+#Adding to Database
+collection.insert_many(parse_json) #parse_json should return list of documents and each new document is defined by the "event" key. i.e. "event": {"date": "-300", "description": "Auge de Meroe", "lang": "ca", "granularity": "year"} and "event": {"date": "-300", "description": "Primera publicaci\u00f3 d'una previsi\u00f3 meteorol\u00f2gica documentada (obra de Teofrast)" will be two separate documents
 
+#Searching by date
+def date(d):
+    c = collection.find({'event.date':d})
+    for i in c:
+        print i
+date('-300')
+
+
+def desc(d):
+    c = collection.find({'event.description':d})
+    for i in c:
+        print i
+desc('Auge de Mero')
+
+
+def gran(g):
+    c = collection.find({'event.granularity':g})
+    for i in c:
+        print i
+gran('year')
+
+
+def lang(l):
+    c = collection.find({'event.lang':l})
+    for i in c:
+        print i
+lang('ca')
+'''
